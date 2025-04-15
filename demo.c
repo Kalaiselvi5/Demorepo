ebb2dfc439162581ed9ea8148912a169d02a6c49
kalaiselvi thanikachalam <t.kalaiselvithanikachalam@gmail.com>
2025-04-15 09:59:40 +0530

    Added contents

--- demo.c
+++ demo.c
@@ -1,2 +1,16 @@
+
+
 Hello to Amazon forest world!!
+
+
+
+
+Products are aligned.
+
+
+DevA  Got a msg from architec that needs to finish my task within 30 mins.
+
+      Deadline has been announced by the project devA needs to take an innediate action on it.
+
+DevB   I have submitted my code to the master branch.
 
